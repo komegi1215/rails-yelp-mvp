6888b49e47ed1f45a65a6aacd0ea3e6c86925530
Megumi Hirao <komegi.blog@gmail.com>
2024-01-09 21:56:48 +0900

    Added model/controllers/views. Working on restaurant views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+puts "cleaning seeds.."
+Restaurant.destroy_all
+
+puts "creating 5 restaurants..."
+
+10.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: Restaurant::CATEGORY.sample
+  )
+end
